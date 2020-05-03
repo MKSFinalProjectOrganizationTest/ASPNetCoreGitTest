7d2eb7f84af8b95b3584d0263e516167170f7b83
MKSFinalProject <62482999+MKSFinalProject@users.noreply.github.com>
2020-05-03 13:59:58 +0300

    Performing Migrations

---
+++ ASPNetCoreGitTest/Migrations/20200501142042_Populating.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASPNetCoreGitTest.Migrations
+{
+    public partial class Populating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert Into TestModels (Name) Values ('Model1'), ('Model2')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ ASPNetCoreGitTest/Models/AppContext.cs
@@ -0,0 +1,23 @@
+﻿using ASPNetCoreGitTest.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreGitTest
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<TestModel> TestModels { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = "data source=.\\SQLEXPRESS; initial catalog=TestDB; integrated security=SSPI";
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
